dc17e831500dc204e100a805e104ea80039b68a9
zsytssk <zsytssk@gmail.com>
2020-09-27 18:23:06 +0800

    feature/recharge_opt:[opt] fix bug send roomOut in hall

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -14,8 +14,8 @@ function createLog(type?: string) {
 }
 
 export let log: typeof console.log = console.log;
-export let debug: typeof console.warn = console.log;
-export let error: typeof console.error = console.log;
+export let debug: typeof console.warn = console.warn;
+export let error: typeof console.error = console.error;
 setImmediate(() => {
     log = createLog();
     debug = createLog('debug');
