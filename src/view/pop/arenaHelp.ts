d63c9221c02d7cec0936979ccc8645d9e6aa579f
zsytssk <zsytssk@gmail.com>
2021-12-22 14:59:34 +0800

    test/game_v2:

--- src/view/pop/arenaHelp.ts
+++ src/view/pop/arenaHelp.ts
@@ -5,6 +5,7 @@ import { Handler } from 'laya/utils/Handler';
 import { offLangChange, onLangChange } from '@app/ctrl/hall/hallCtrlUtil';
 import { ui } from '@app/ui/layaMaxUI';
 import { getAllChildren } from '@app/utils/layaQueryElements';
+import { resizeContain } from '@app/utils/layaUtils';
 import { tplStr } from '@app/utils/utils';
 
 import { arenaGetRuleData } from './popSocket';
@@ -46,7 +47,7 @@ export default class ArenaHelpPop
         try {
             const boxList = await getAllChildren(tabBody);
             const {
-                roomConfig: { freeNum, everydayConfig },
+                roomConfig: { freeNum },
                 globalConfig: {
                     initBulletNum,
                     rankingScoreDown,
@@ -65,12 +66,14 @@ export default class ArenaHelpPop
             labels1[0].text = tplStr('arenaHelpRule11', { deadlineTime });
             labels1[1].text = tplStr('arenaHelpRule12', { freeNum });
             labels1[2].text = tplStr('arenaHelpRule13');
+            resizeContain(boxList[0], 10, 'vertical');
 
             const labels2 = getAllChildren(boxList[1]) as Label[];
             labels2[0].text = tplStr('arenaHelpRule21', { initBulletNum });
             labels2[1].text = tplStr('arenaHelpRule22', {});
             labels2[2].text = tplStr('arenaHelpRule23', {});
             labels2[3].text = tplStr('arenaHelpRule24', {});
+            resizeContain(boxList[1], 10, 'vertical');
 
             const labels3 = getAllChildren(boxList[2]) as Label[];
             labels3[0].text = tplStr('arenaHelpRule31', { rankingScoreDown });
@@ -78,6 +81,7 @@ export default class ArenaHelpPop
             labels3[2].text = tplStr('arenaHelpRule33', {});
             labels3[3].text = tplStr('arenaHelpRule34', {});
             labels3[4].text = tplStr('arenaHelpRule35', {});
+            resizeContain(boxList[2], 10, 'vertical');
 
             const labels4 = getAllChildren(boxList[3]) as Label[];
             labels4[0].text = tplStr('arenaHelpRule41', {});
@@ -88,6 +92,7 @@ export default class ArenaHelpPop
                 gun1004,
                 gun1005,
             });
+            resizeContain(boxList[3], 10, 'vertical');
         } catch {}
     }
     public destroy() {
